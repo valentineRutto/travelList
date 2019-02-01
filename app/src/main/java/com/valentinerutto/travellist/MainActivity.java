717bd6a58a844687eba990e834a550dc3a3df8ad
valentinerutto <rutto28@gmail.com>
2019-02-01 16:38:22 +0300

    -add bottomappbar

--- app/src/main/java/com/valentinerutto/travellist/MainActivity.java
+++ app/src/main/java/com/valentinerutto/travellist/MainActivity.java
@@ -1,7 +1,12 @@
 package com.valentinerutto.travellist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        BottomAppBar bottomAppBar = findViewById(R.id.fab);
+//        bottomAppBar.replaceMenu(R.menu.menu);
+//        bottomAppBar.setNavigationOnClickListener{
+//            // do something interesting on navigation click
+//        }
 
     }
 }
