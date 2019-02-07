d78462fea3640eb6958a3d92a9a809f698d5e23c
valentinerutto <rutto28@gmail.com>
2019-02-07 15:30:52 +0300

    -add firebase and taskitemmodel

---
+++ app/src/main/java/com/valentinerutto/travellist/taskItem.java
@@ -0,0 +1,41 @@
+package com.valentinerutto.travellist;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class taskItem implements Serializable {
+private String task,country,date;
+    public taskItem() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public HashMap<String,String> toFirebaseObject(){
+        HashMap<String,String> todo = new HashMap<String, String>();
+        todo.put("task",task);
+        return todo;
+    }
+
+}
--- app/src/main/java/com/valentinerutto/travellist/todo_add_new.java
+++ app/src/main/java/com/valentinerutto/travellist/todo_add_new.java
@@ -1,13 +1,40 @@
 package com.valentinerutto.travellist;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class todo_add_new extends AppCompatActivity {
+    FloatingActionButton fab;
+    EditText task,taskdate;
+    Spinner country;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_add_new);
+   fab = findViewById(R.id.fab_Add);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveTodo();
+            }
+        });
     }
+
+    private void saveTodo() {
+        task=findViewById(R.id.task_name);
+        taskdate=findViewById(R.id.task_date);
+//        make datepicker visible on taskdateclick
+        datePicker=findViewById(R.id.datepicker);
+
+
+
+    }
+
 }
