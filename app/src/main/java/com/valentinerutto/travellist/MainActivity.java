c4b03fe53fcab63f0afb2d79f64e3331f28716f4
valentinerutto <rutto28@gmail.com>
2019-02-27 16:53:43 +0300

    -refresh data on resume

--- app/src/main/java/com/valentinerutto/travellist/MainActivity.java
+++ app/src/main/java/com/valentinerutto/travellist/MainActivity.java
@@ -1,15 +1,30 @@
 package com.valentinerutto.travellist;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+RecyclerViewAdapter adapter;
+ArrayList<taskItem> todolist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,45 @@ public class MainActivity extends AppCompatActivity {
 //        bottomAppBar.setNavigationOnClickListener{
 //            // do something interesting on navigation click
 //        }
+        todolist = new ArrayList<>();
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        adapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+        firebaseDatabase.getReference("todoList").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                todolist.clear();
+
+                Log.w("TodoApp", "getUser:onCancelled " + dataSnapshot.toString());
+                Log.w("TodoApp", "count = " + String.valueOf(dataSnapshot.getChildrenCount()) + " values " + dataSnapshot.getKey());
+
+
+                for (DataSnapshot data : dataSnapshot.getChildren()){
+                    taskItem task = data.getValue(com.valentinerutto.travellist.taskItem.class);
+                    todolist.add(task);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("TodoApp","getUser:onCancelled",databaseError.toException());
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/valentinerutto/travellist/RecyclerViewAdapter.java
@@ -0,0 +1,25 @@
+package com.valentinerutto.travellist;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class RecyclerViewAdapter  extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
